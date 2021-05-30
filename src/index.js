cf807cc7df7e9b3c8f6ef554729fb64a3770ff38
megan-createsit <mdix5@outlook.com>
2021-05-30 12:07:37 -0500

    Add Forecast - correct

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ let days = [
   "Wednesday",
   "Thursday",
   "Friday",
-  "Saturday"
+  "Saturday",
 ];
 let day = days[now.getDay()];
 dateTime.innerHTML = `${day}, ${currentTime}`;
@@ -21,7 +21,7 @@ function searchSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#text");
   let currentCity = document.querySelector("h1");
-  currentCity.innerHTML = `${city.value}`
+  currentCity.innerHTML = `${city.value}`;
   let apiKey = `128f6df614a32ed62271bc180835bf6d`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&units=imperial`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(currentTemp);
@@ -30,20 +30,30 @@ let search = document.querySelector("#city-search");
 search.addEventListener("submit", searchSubmit);
 //end search city info//
 //start forecast info//
+function formatForecast(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = [
+    "Sun",
+    "Mon",
+    "Tues",
+    "Wed",
+    "Thurs",
+    "Fri",
+    "Sat",
+  ];
+  return days[date.getDay()];
+}
 function displayForecast(response) {
   let forecast = response.data.daily;
-
   let forecastElement = document.querySelector("#forecast");
-
   let forecastHTML = `<div class="row">`;
-
   forecast.forEach(function (forecastDay, index) {
     if (index < 6) {
       forecastHTML =
         forecastHTML +
         `
       <div class="col-2">
-        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <div class="weather-forecast-date">${formatForecast(forecastDay.dt)}</div>
         <img
           src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
@@ -54,16 +64,15 @@ function displayForecast(response) {
         <div class="weather-forecast-temperatures">
           <span class="weather-forecast-temperature-max"> ${Math.round(
             forecastDay.temp.max
-          )}°C </span>
-          <span class="weather-forecast-temperature-min"> ${Math.round(
+          )}°F </span>
+          <span class="weather-forecast-temperature-min"> | ${Math.round(
             forecastDay.temp.min
-          )}°C </span>
+          )}°F </span>
         </div>
       </div>
   `;
     }
   });
-
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
@@ -90,34 +99,33 @@ function currentTemp(response) {
   tempReplace.innerHTML = `${imperialTemperature}°F`;
   precipReplace.innerHTML = `Humidity: ${precip}%`;
   windReplace.innerHTML = `Wind: ${wind} m/h`;
-  iconReplace.setAttribute("src", `http://openweathermap.org/img/wn/${icon}@2x.png`);
+  iconReplace.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${icon}@2x.png`
+  );
   iconReplace.setAttribute("alt", response.data.weather[0].description);
-  function imperialChange(event) {
-    event.preventDefault();
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active");
-    let temperatureElement = document.querySelector("b");
-    let imperialRead = Math.round(imperialTemperature);
-    temperatureElement.innerHTML = `${imperialRead}°F`;
-
-    getForecast(response.data.coord);
-  }
-  
-  function metricChange(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let metricTemperature = (imperialTemperature - 32) * 5/9;
-    let temperatureElement = document.querySelector("b");
-    let metricRead = Math.round(metricTemperature)
-    temperatureElement.innerHTML = `${metricRead}°C`;
-  }
-  
+  getForecast(response.data.coord);
+}
+function imperialChange(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("b");
+  let imperialRead = Math.round(imperialTemperature);
+  temperatureElement.innerHTML = `${imperialRead}°F`;
+}
+function metricChange(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let metricTemperature = ((imperialTemperature - 32) * 5) / 9;
+  let temperatureElement = document.querySelector("b");
+  let metricRead = Math.round(metricTemperature);
+  temperatureElement.innerHTML = `${metricRead}°C`;
+}
 let fahrenheitLink = document.querySelector("#fahrenheit");
 fahrenheitLink.addEventListener("click", imperialChange);
-
 let celsiusLink = document.querySelector("#celsius");
 celsiusLink.addEventListener("click", metricChange);
-}
 let imperialTemperature = null;
 //end today weather data info//
